7b7d7ba03fd3310eb01f68cd375f9624558ea152
DavidMart199 <omar36453670@gmail.com>
2020-03-04 15:57:51 -0500

    pantalla de configuracion
    ui

--- Assets/Scenes/Scenes.cs
+++ Assets/Scenes/Scenes.cs
@@ -5,18 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class Scenes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void pantallaPrincipal()
     {
-        
+        SceneManager.LoadScene(1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Arcade()
     {
-        if (Input.GetMouseButton(0))
-        {
-            SceneManager.LoadScene(1);
-        }
+        SceneManager.LoadScene(2);
+    }
+    public void OnevsOne()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Configuracion()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Controles()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Pantalla()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Sonido()
+    {
+        SceneManager.LoadScene(7);
     }
 }
