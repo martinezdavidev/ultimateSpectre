4439eb7314c0bfb4d75a004bc4f86f2b6400949e
DavidMart199 <omar36453670@gmail.com>
2020-03-06 13:12:33 -0500

    esenas de confi "listas"

--- Assets/Scenes/Scenes.cs
+++ Assets/Scenes/Scenes.cs
@@ -5,32 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class Scenes : MonoBehaviour
 {
-    public void pantallaPrincipal()
+    public void pantallaPrincipal() // Carga la esena principal del juego -tambien se usa para volver atras desde confi
     {
         SceneManager.LoadScene(1);
     }
-    public void Arcade()
+    public void Arcade() // Carga la esena seleccion de personajes modo arcade (un jugador) vs UPC 
     {
         SceneManager.LoadScene(2);
     }
-    public void OnevsOne()
+    public void OnevsOne() // Carga la esena seleccion de personajes modo 1 vs 1 
     {
         SceneManager.LoadScene(3);
     }
-    public void Configuracion()
+    public void Configuracion() // Carga la esena comfiguracion-tambien se usa para volver atras desde controles,pantalla,sonido e idioma
     {
         SceneManager.LoadScene(4);
     }
-    public void Controles()
+    public void Controles() // Carga la esena comfiguracion de controles
     {
         SceneManager.LoadScene(5);
     }
-    public void Pantalla()
+    public void Pantalla() // Carga la esena comfiguracion de pantalla, resolucion
     {
         SceneManager.LoadScene(6);
     }
-    public void Sonido()
+    public void Sonido() // Carga la esena comfiguracion de sonido, volumen de la musica efectos etc...
     {
         SceneManager.LoadScene(7);
+    }
+    public void Idioma() // Carga la esena comfiguracion de indioma, cambia de ingles a español 
+    {
+        SceneManager.LoadScene(8);
     }
 }
---
+++ Assets/Scenes/UGUIMenu.cs
@@ -0,0 +1,51 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Esta clase no pertenece al cualquier proyecto en el cual se encuentre presente, es una funcionalidad del motor en si, mas no del proyecto en el que se encuentre, por lo cual no es propiedad del proyecto. 
+/// es propiedad de Rodrigo Porras M. 
+/// </summary>
+public class UGUIMenu : MonoBehaviour{
+
+	[MenuItem ("UI/Anchor Around Object")]
+
+	static void uGUIAnchorAroundObject(){
+
+		//var o = Selection.activeGameObject;
+		GameObject[] array = Selection.gameObjects;
+
+		foreach (var item in array) {
+			if (item != null && item.GetComponent<RectTransform>() != null){
+				
+				var r = item.GetComponent<RectTransform>();
+				var p = item.transform.parent.GetComponent<RectTransform>();
+				
+				var offsetMin = r.offsetMin;
+				var offsetMax = r.offsetMax;
+				var _anchorMin = r.anchorMin;
+				var _anchorMax = r.anchorMax;
+				
+				var parent_width = p.rect.width;      
+				var parent_height = p.rect.height;  
+				
+				var anchorMin = new Vector2(_anchorMin.x + (offsetMin.x / parent_width),
+				                            _anchorMin.y + (offsetMin.y / parent_height));
+				var anchorMax = new Vector2(_anchorMax.x + (offsetMax.x / parent_width),
+				                            _anchorMax.y + (offsetMax.y / parent_height));
+				
+				r.anchorMin = anchorMin;
+				r.anchorMax = anchorMax;
+				
+				r.offsetMin = new Vector2(0, 0);
+				r.offsetMax = new Vector2(0, 0);
+				r.pivot = new Vector2(0.5f, 0.5f);
+				
+			}
+		}
+
+
+	}
+}
+#endif
